14beadf3fc376e17b384304c0aedeaba59496b5d
TanyaLnv <115620110+TanyaLnv@users.noreply.github.com>
2023-01-17 19:29:27 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -39,6 +39,40 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+
+    let doz = (expr.length / 10);
+    let str = "";
+    let word = "";
+    for(let i = 0; i <= doz; i++){
+        if(expr[i*10] === '*'){
+            str = `${str}${word} `; 
+            word = "";
+        }
+        else{
+            let l = "";
+            for(let j = 10*i + 1; j < 10 * (i+1); j+=2)
+            {
+                let bin = `${expr[j-1]}${expr[j]}`;
+                if(bin === '10')
+                {
+                l = `${l}.`;
+             }
+             else if(bin === '11'){
+                l= `${l}-`;
+            }
+            }
+            let lett = "";
+            for(let letter in MORSE_TABLE){
+                if(letter === l){
+                  lett = `${MORSE_TABLE[letter]}`;
+             }
+        }
+        word = `${word}${lett}`;
+    }
+}
+
+    str = `${str}${word}`;
+    return str;
 }
 
 module.exports = {
